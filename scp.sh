0701a81aa78f726ba2774a6cfb30f63e3e18258c
tamimcse <tamim@csebuet.org>
2019-05-02 18:01:51 -0400

    .

---
+++ scp.sh
@@ -0,0 +1,6 @@
+sudo scp /home/tamim/website/index.html mislam4@web.cs.kent.edu:/users/kent/mislam4/public_html
+sudo scp /home/tamim/website/rcp-using-domino.html mislam4@web.cs.kent.edu:/users/kent/mislam4/public_html
+sudo scp /home/tamim/website/sail-in-linux-kernel.html mislam4@web.cs.kent.edu:/users/kent/mislam4/public_html
+sudo scp /home/tamim/website/papers.html mislam4@web.cs.kent.edu:/users/kent/mislam4/public_html
+sudo scp /home/tamim/website/resume.pdf mislam4@web.cs.kent.edu:/users/kent/mislam4/public_html
+sudo scp /home/tamim/website/nc-tcp-slide.pdf mislam4@web.cs.kent.edu:/users/kent/mislam4/public_html
